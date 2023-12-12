6baadc67eb25c5cd38ba6d6fd5dc4aa91753bb68
Marcus Behel <marcus.behel@mindspring.com>
2023-12-12 02:16:38 -0500

    Debugging bash color issues

--- bash/custom/themes/custom/custom.theme.sh
+++ bash/custom/themes/custom/custom.theme.sh
@@ -1,24 +1,28 @@
 #! bash oh-my-bash.module
 
-PS1="\$(_omb_theme_prompt_arrow)"
+# Note: Use _omb_prompt_... in PS1
+# If ehco / printf ing use _omb_term_...
+
+PS1=""
+# PS1="\$(_omb_theme_prompt_arrow)"
 if [[ -f /etc/debian_chroot ]]; then
     chroot_name=$(cat /etc/debian_chroot)
     PS1+="($chroot_name)"
 fi
 # TODO: venv
-PS1+="[${_omb_prompt_bold_green}\u@\h:${_omb_prompt_bold_navy}\W${_omb_term_reset_color}${_omb_term_normal}]"
-PS1+="\$(scm_prompt_info)"
+PS1+="[${_omb_prompt_bold_green}\u@\h:${_omb_prompt_bold_navy}\W${_omb_prompt_normal}]"
+# PS1+="\$(scm_prompt_info)"
 PS1+="\$ "
 
-SCM_THEME_PROMPT_PREFIX="(${_omb_prompt_cyan}"
-SCM_THEME_PROMPT_SUFFIX="${_omb_term_reset_color}${_omb_term_normal})"
-SCM_THEME_PROMPT_DIRTY=" ${_omb_prompt_red}✗"
+SCM_THEME_PROMPT_PREFIX="(${_omb_term_cyan}"
+SCM_THEME_PROMPT_SUFFIX="${_omb_term_normal})"
+SCM_THEME_PROMPT_DIRTY=" ${_omb_term_red}✗"
 SCM_THEME_PROMPT_CLEAN=""
 
 function _omb_theme_prompt_arrow {
     if [ $? -ne 0 ]; then
-        echo "${_omb_term_bold_red}➜${_omb_term_reset_color}${_omb_term_normal}"
+        echo -ne "${_omb_term_bold_red}➜${_omb_term_normal}"
     else
-        echo "${_omb_term_bold_green}➜${_omb_term_reset_color}${_omb_term_normal}"
+        echo -ne "${_omb_term_bold_green}➜${_omb_term_normal}"
     fi
 }
