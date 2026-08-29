b2a25fc0041317a02eb3fc93d68f7cbbb2f95342
Marcus Behel <marcus.behel@mindspring.com>
2026-08-29 16:30:54 -0400

    Add git status to pwsh prompt

--- install_pwsh.ps1
+++ install_pwsh.ps1
@@ -4,6 +4,7 @@
 # since it gets synced on windows. So no need to implement the template system like install.sh
 # has. Can just always copy the latest one
 
+[System.IO.Directory]::CreateDirectory((Split-Path $PROFILE)) | Out-Null
 cp $PSScriptRoot/template/Microsoft.PowerShell_profile.ps1 $PROFILE
 if (-not (Test-Path -Path "$HOME/.pwsh_profile.ps1")) {
     New-Item -Path "$HOME/.pwsh_profile.ps1" -ItemType "File" -Value "# System specifics go below`n"
--- pwsh_profile.ps1
+++ pwsh_profile.ps1
@@ -2,11 +2,36 @@
 
 
 
-# TODO: Make windows terminal duplicate tab work
+# TODO: Make windows terminal duplicate tab work including in WSL
 
 
 
 # Custom prompt. Matches bash / zsh prompts, but uses '>' at the end not '$' or '%'
+function DoPromptGitInfo {
+    # Minimize git command invocations because launching git on windows is slow
+    # enough to be noticeable. Use one git status command to get all info needed for prompt
+    # Not using posh-git because I use this in contexts where I can't install modules
+    $git_banch = ""
+    $git_dirty = ""
+    git status --porcelain=v2 --branch | ForEach-Object {
+        $line_type, $line_contents, $line_contents2 = $_ -split '\s+', 3
+        if($line_type -eq "#") {
+            if($line_contents -eq "branch.oid"){
+                $git_branch = $line_contents2.Substring(0,7)
+            } elseif(($line_contents -eq "branch.head") -and -not ($line_contents2 -eq "(detached)")){
+                $git_branch = $line_contents2
+            }
+        } elseif ($line_type -in @("1", "2", "u", "?")) {
+            $git_dirty = " ✗"
+        }
+    }
+    if($git_branch){
+        Write-Host -NoNewline "("
+        Write-Host -NoNewline -ForegroundColor Cyan $git_branch
+        Write-Host -NoNewline -ForegroundColor Red $git_dirty
+        Write-Host -NoNewline ")"
+    }
+}
 function prompt {
     # Green / red arrow depending on last command exit code
     $lastSuccess = $global:?
@@ -28,8 +53,9 @@ function prompt {
     Write-Host "$curloc" -NoNewline -ForegroundColor Blue
     Write-host "]" -NoNewline
 
-    # TODO: git status prompt info (not using posh-git b/c need this in places I can't install modules)
-
+    # Git status portion
+    DoPromptGitInfo
+    
     # End of prompt right before command is typed
     return "> "
 }
@@ -44,7 +70,14 @@ function which{
 function ln ($target, $link) {
 	New-Item -Path $link -ItemType SymbolicLink -Value $target
 }
+# TODO: Vim alias to vimx or gvim -v
+# TODO: fixssh aliases
+# TODO: Trash aliases
+# TODO: openwin for WSL
 
+
+# Env vars
+# TODO: set editor
 
 
 # Better tab completion and history search
@@ -55,3 +88,4 @@ Set-PSReadLineOption -HistorySearchCursorMovesToEnd:$true
 Set-PSReadLineOption -ShowToolTips:$False
 Set-PSReadLineOption -PredictionSource None
 Set-PSReadlineOption -BellStyle None
+
